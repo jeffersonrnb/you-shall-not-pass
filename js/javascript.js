22974426fe98cae2d96c50c3450007b46a1ceb58
Jefferson Brito <jeffersonbrito@infojr.com.br>
2015-01-11 22:56:49 -0300

    Adiciona Habilidade e Poder de Fogo
    Refs #1

--- js/javascript.js
+++ js/javascript.js
@@ -2,19 +2,9 @@ window.interval = null;
 
 window.addEventListener('load', function () {
     'use strict'
-    
+
     var char1, char2;
 
-    var Character = function (attributes) {
-        this.avatar = attributes.avatar;
-        this.name = attributes.name;
-        this.strength = attributes.strength;
-        this.armor = attributes.armor;
-        this.resistence = attributes.resistence;
-        this.life = (attributes.resistence * 5) === 0 ? 1 : (attributes.resistence * 5);
-        this.status = '';
-    }
-    
     var simulador = function (f1, a2) {
         if (f1 > a2) {
             return 'First';
@@ -22,54 +12,23 @@ window.addEventListener('load', function () {
             return 'Second';
         }
     };
-    
-    var printChar = function(char) {
-        var body = document.querySelector('body');
+
+    var printChar = function(character) {
+        var battle = document.querySelector('.battle');
 
         var request = new XMLHttpRequest();
         request.open('GET', 'templateChar.html', true);
         request.addEventListener('load', function () {
-            var template = request.responseText.replace('{{AVATAR}}', char1.avatar).replace('{{NAME}}', char1.name).replace('{{STRENGTH}}', char1.strength).replace('{{ARMOR}}', char1.armor).replace('{{RESISTENCE}}', char1.resistence).replace('{{LIFE}}', char1.life);
-        
-            body.innerHTML += template;
+            var template = request.responseText.replace('{{AVATAR}}', character.avatar).replace('{{NAME}}',
+            character.name).replace('{{STRENGTH}}', character.strength).replace('{{ARMOR}}',
+            character.armor).replace('{{ABILITY}}', character.ability).replace('{{FIREPOWER}}',
+            character.firepower).replace('{{RESISTENCE}}', character.resistence).replace('{{LIFE}}', character.life);
+
+            battle.innerHTML += template;
             document.querySelector('.button').addEventListener('click', atacar);
         });
-        
+
         request.send();
-        
-        /*var divCharacter = document.createElement('div');
-        divCharacter.classList.add('character');
-        body.appendChild(divCharacter);
-
-        node = document.createElement('div');
-        node.classList.add('name');
-        name = document.createTextNode('Nome: ' + char.name);
-        divCharacter.appendChild(node);
-        node.appendChild(name);
-
-        node = document.createElement('div');
-        node.classList.add('strength');
-        strength = document.createTextNode('Strength: ' + char.strength);
-        divCharacter.appendChild(node);
-        node.appendChild(strength);
-
-        node = document.createElement('div');
-        node.classList.add('armor');
-        armor = document.createTextNode('Armor: ' + char.armor);
-        divCharacter.appendChild(node);
-        node.appendChild(armor);
-
-        node = document.createElement('div');
-        node.classList.add('resistence');
-        resistence = document.createTextNode('Resistence: ' + char.resistence);
-        divCharacter.appendChild(node);
-        node.appendChild(resistence);
-
-        node = document.createElement('div');
-        node.classList.add('life');
-        life = document.createTextNode('Life: ' + char.life);
-        divCharacter.appendChild(node);
-        node.appendChild(life);*/
     }
 
     var createCharacter = function (nome) {
@@ -78,16 +37,18 @@ window.addEventListener('load', function () {
     }
 
     var generateChar = function (name) {
-        var countChar, limite = 10, strength, armor, resistence;
+        var countChar, limite = 20, strength, armor, ability, firepower, resistence;
 
         countChar = limite;
 
         strength = Math.ceil(Math.random() * 5);
         armor = Math.ceil(Math.random() * 5);
-        countChar = countChar - (strength + armor);
+        ability = Math.ceil(Math.random() * 5);
+        firepower = Math.ceil(Math.random() * 5);
+        countChar = countChar - (strength + armor + ability + firepower);
         resistence = Math.max(countChar, 0) <= 5 ?  Math.max(countChar, 0) : 5;
 
-        var char = new Character({name: name, strength: strength, armor: armor, resistence: resistence, avatar: 'arch-mage'});
+        var char = new Character({name: name, strength: strength, armor: armor, ability: ability, firepower: firepower, resistence: resistence, avatar: 'img/characters/arch-mage'});
 
         char.life = (char.resistence * 5) === 0 ? 1 : (char.resistence * 5);
 
@@ -133,20 +94,21 @@ window.addEventListener('load', function () {
         var totalDamage;
         var avatars = document.querySelectorAll('.avatar');
         avatars.item(0).classList.add('normal');
+        avatars.item(1).classList.add('normal');
         if (char1.life > 0 && char2.life > 0) {
             totalDamage = (char1.strength * Math.ceil(Math.random() * 6)) - (char2.armor * Math.ceil(Math.random() * 6));
             if (totalDamage > 0) {
-                interval = setInterval(function(){ 
+                interval = setInterval(function(){
                     if (avatars.item(0).classList.contains('normal')) {
                         avatars.item(0).classList.add('atk-1');
                         avatars.item(0).classList.remove('normal');
-                        avatars[0].src = 'arch-mage-attack-staff-1.png';
+                        avatars[0].src = 'img/characters/arch-mage-attack-staff-1.png';
                     } else if (avatars.item(0).classList.contains('atk-1')) {
                         avatars.item(0).classList.remove('atk-1');
                         avatars.item(0).classList.add('atk-2');
-                        avatars[0].src = 'arch-mage-attack-staff-2.png';
+                        avatars[0].src = 'img/characters/arch-mage-attack-staff-2.png';
                     } else {
-                        avatars[0].src = 'arch-mage.png';
+                        avatars[0].src = 'img/characters/arch-mage.png';
                         interval = null;
                     }}, 150);
                 char2.life = char2.life - totalDamage;
@@ -154,12 +116,20 @@ window.addEventListener('load', function () {
             if(char2.life > 0) {
                 totalDamage = (char2.strength * Math.ceil(Math.random() * 6)) - (char1.armor * Math.ceil(Math.random() * 6));
                 if (totalDamage > 0) {
-                    var i; 
-                    i = setInterval(function(){ avatars[1].src = 'arch-mage-attack-staff-1.png'; }, 1000);
-                    i = setInterval(function(){ avatars[1].src = 'arch-mage-attack-staff-2.png'; }, 1000);
+                    interval = setInterval(function(){
+                    if (avatars.item(1).classList.contains('normal')) {
+                        avatars.item(1).classList.add('atk-1');
+                        avatars.item(1).classList.remove('normal');
+                        avatars[1].src = 'img/characters/arch-mage-attack-staff-1.png';
+                    } else if (avatars.item(1).classList.contains('atk-1')) {
+                        avatars.item(1).classList.remove('atk-1');
+                        avatars.item(1).classList.add('atk-2');
+                        avatars[1].src = 'img/characters/arch-mage-attack-staff-2.png';
+                    } else {
+                        avatars[1].src = 'img/characters/arch-mage.png';
+                        interval = null;
+                    }}, 150);
                     char1.life = char1.life - totalDamage;
-                    i = setInterval(function(){ avatars[1].src = 'arch-mage.png'; }, 1000);
-                    i = null;
                 }
             }
         }
@@ -178,29 +148,34 @@ window.addEventListener('load', function () {
             divCharacter.style.backgroundColor = 'green';
         }
     }
-    
+
     var submitFormChar = function(event) {
         event.preventDefault();
-        var countChar, limite = 10, name, strength, armor, resistence;
+        var countChar, limite = 20, name, strength, armor, ability, firepower, resistence;
 
         countChar = limite;
-        
+
         name = document.querySelector('#char_name').value;
         strength = parseInt(document.querySelector('#char_strength').value) > 5 ? 5 : parseInt(document.querySelector('#char_strength').value);
         armor = parseInt(document.querySelector('#char_armor').value) > 5 ? 5 : parseInt(document.querySelector('#char_armor').value);
-        countChar = countChar - (strength + armor);
+        ability = parseInt(document.querySelector('#char_ability').value) > 5 ? 5 : parseInt(document.querySelector('#char_ability').value);
+        firepower = parseInt(document.querySelector('#char_firepower').value) > 5 ? 5 : parseInt(document.querySelector('#char_firepower').value);
+
+        countChar = countChar - (strength + armor + ability + firepower);
         resistence = Math.max(countChar, 0) <= 5 ? Math.max(countChar, 0) : 5;
 
-        char1 = new Character({name: name, strength: strength, armor: armor, resistence: resistence, avatar: 'arch-mage'});
-        
+        char1 = new Character({name: name, strength: strength, armor: armor, ability: ability, firepower: firepower, resistence: resistence, avatar: 'img/characters/arch-mage'});
+
         document.querySelector('#form_char1').remove();
-        
+
         char2 = createCharacter('Personagem2');
-        
+
+        document.querySelector('.battle').style.display = 'block';
+
         printChar(char1);
         printChar(char2);
     }
-    
+
     document.querySelector('.button').addEventListener('click', atacar);
     document.querySelector('#form_char1').addEventListener('submit', submitFormChar);
 
